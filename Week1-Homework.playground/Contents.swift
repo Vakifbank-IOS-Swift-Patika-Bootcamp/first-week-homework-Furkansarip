12fb4aded791c1acc390dd526c7cd0e20e664c78
Furkan Sarı <furkansarip@gmail.com>
2022-11-01 22:57:08 +0300

    1-2-3-4 Question Solutions

---
+++ Week1-Homework.playground/Contents.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+
+//Question 1
+print("Question 1 Answer")
+print("-------------------")
+let value = "tet"
+func palindromeChecker(to value : String){
+    if value == String(value.reversed()){
+        print("This is a palindrome string.")
+    }
+    else {
+        print("This is not a palindrome!")
+    }
+}
+
+palindromeChecker(to: value)
+print("\nQuestion 2 Answer")
+print("-------------------")
+//Question 2
+let items = [1,1,1,2,3,2,3,1]
+struct T : Hashable {
+
+}
+func calculator<T>(datas : [T]) -> [T:Int]{
+let mapDatas = datas.map{($0,1)}
+let counts = Dictionary(mapDatas, uniquingKeysWith: +)
+return counts
+}
+
+print(calculator(datas: items))
+
+print("\nQuestion 3 Answer")
+print("-------------------")
+var pyramidVal = String()
+func createPyramid(pow : Int){
+    var symbolCount = 0
+    for _ in 1...pow {
+       
+        for _ in 0...symbolCount {
+            pyramidValue += "*"
+          
+        }
+        pyramidValue += "\n"
+        symbolCount += 1
+    }
+    
+    print(pyramidValue)
+}
+
+createPyramid(pow: 4)
+
+print("\nQuestion 4 Answer")
+print("-------------------")
+
+var pyramidValue = String()
+func createAnPyramid(pow : Int){
+    var spacerCount = pow
+    var symbolCount = 0
+    for _ in 1...pow {
+        
+        for _ in 0..<spacerCount-1 {
+            pyramidValue += " "
+        }
+       
+        for _ in 0...symbolCount {
+            pyramidValue += "* "
+          
+        }
+        pyramidValue += "\n"
+        spacerCount -= 1
+        symbolCount += 1
+    }
+    
+    print(pyramidValue)
+}
+
+createAnPyramid(pow: 4)
+
